84307a8e5fcd5069259e104f25a2c15e58a2fa96
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-04-05 21:19:59 -0400

    Update ball script to check for collisions between the top, bottom and paddle

--- Pong/Assets/Project/Scripts/Ball.cs
+++ Pong/Assets/Project/Scripts/Ball.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    public float speed = 1f;
+    private Rigidbody2D ballRigidbody;
+
 	// Use this for initialization
 	void Start () {
-		
+        ballRigidbody = GetComponent<Rigidbody2D>();
+        ballRigidbody.velocity = new Vector2(-0.5f, speed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.tag == "Limit")
+        {
+            if (otherCollider.transform.position.y > transform.position.y && ballRigidbody.velocity.y > 0)
+            {
+                ballRigidbody.velocity = new Vector2(ballRigidbody.velocity.x, -ballRigidbody.velocity.y);
+            }
+
+            if (otherCollider.transform.position.y < transform.position.y && ballRigidbody.velocity.y < 0)
+            {
+                ballRigidbody.velocity = new Vector2(ballRigidbody.velocity.x, -ballRigidbody.velocity.y);
+            }
+        }
+        else if (otherCollider.tag == "Paddle")
+        {
+            if (otherCollider.transform.position.x < transform.position.x && ballRigidbody.velocity.x < 0)
+            {
+                ballRigidbody.velocity = new Vector2(-ballRigidbody.velocity.x, ballRigidbody.velocity.y);
+            }
+
+            if (otherCollider.transform.position.x > transform.position.x && ballRigidbody.velocity.x > 0)
+            {
+                ballRigidbody.velocity = new Vector2(-ballRigidbody.velocity.x, ballRigidbody.velocity.y);
+            }
+        }
+    }
 }
